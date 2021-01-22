255c2134cd8c0cf6135fe33f527a318d779d202e
Paul Weikel <pweikel@nd.edu>
2021-01-22 11:38:36 -0500

    Added GoogleVerification component

---
+++ src/@ndlib/gatsby-theme-marble/components/Pages/IndexPage/GoogleVerification/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Helmet from 'react-helmet'
+
+export const GoogleVerification = () => {
+  return (
+    <Helmet><meta name='google-site-verification' content='i5bUlLd7k9T3I_XoLupYuPOrZGkJ8BMSX9EIcEXjU0w' />
+    </Helmet>
+  )
+}
+
+export default GoogleVerification

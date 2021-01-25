6f1e85512ace76aac1bfbb5437963438ab90ff5d
dan <dwolfe2@nd.edu>
2021-01-25 10:00:33 -0500

    Adding additional verifications

--- src/@ndlib/gatsby-theme-marble/components/Pages/IndexPage/GoogleVerification/index.js
+++ src/@ndlib/gatsby-theme-marble/components/Pages/IndexPage/GoogleVerification/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React from 'react'
 import Helmet from 'react-helmet'
 
 export const GoogleVerification = () => {
   return (
     <Helmet>
-      <meta 
-        name='google-site-verification' 
+      <meta
+        name='google-site-verification'
         content='i5bUlLd7k9T3I_XoLupYuPOrZGkJ8BMSX9EIcEXjU0w'
         userId='pw'
+      />
+      <meta
+        name='google-site-verification'
+        content='4G8TYzyR2-8KWRdA3F1LRXv9l1_sBvV3YS4UZ7115Po'
+        user='mk'
+      />
+      <meta
+        name='google-site-verification' content='qq7fBdp8zPSI3R9_LFQ4YFmBzTw7Etg5M5HbSgZ_hak'
+        user='rf'
+      />
+      <meta
+        name='google-site-verification'
+        content='zISdamiilUYTQJfVqU4OSM85zEDAJTcioljTdgbaCxo'
+        user='jh'
+      />
+      <meta
+        name='google-site-verification'
+        content='df7oJV_ryb0hmj991vCUHHgEkZs4_PxsqKbobpHEp8U'
+        user='rd'
+      />
+      <meta
+        name='google-site-verification'
+        content='kiUlnvDP0sZ96LCd5lRAbBvMxYVje53ppg2omqweX2Q'
+        user='dw'
       />
     </Helmet>
   )

c1ebd4183f390dce5ce1ddf5a28c766a2b854294
Paul Weikel <pweikel@nd.edu>
2021-02-02 14:22:53 -0500

    fixed path

--- src/@ndlib/gatsby-theme-marble/components/Layout/PageWrapper/index.js
+++ src/@ndlib/gatsby-theme-marble/components/Layout/PageWrapper/index.js
@@ -4,7 +4,7 @@ import SkipToMain from '@ndlib/gatsby-theme-marble/src/components/Layout/PageWra
 import BrandingHeader from '@ndlib/gatsby-theme-marble/src/components/Layout/PageWrapper/BrandingHeader'
 import NavigationHeader from '@ndlib/gatsby-theme-marble/src/components/Layout/PageWrapper/NavigationHeader'
 import Footer from './Footer'
-import FeedbackModal from '../../Internal/FeedbackModal'
+import FeedbackModal from '@ndlib/gatsby-theme-marble/components/Internal/FeedbackModal'
 
 const PageWrapper = ({ children, location }) => {
   return (

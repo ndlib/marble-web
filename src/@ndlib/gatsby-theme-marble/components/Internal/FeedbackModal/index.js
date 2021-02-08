2b1bb5b372e2a62bf5ca2ba24000707ebccc6bd9
jmille79 <jmille79@nd.edu>
2021-02-08 12:31:01 -0500

    MARBLE-1391 Update Feedback Form

--- src/@ndlib/gatsby-theme-marble/components/Internal/FeedbackModal/index.js
+++ src/@ndlib/gatsby-theme-marble/components/Internal/FeedbackModal/index.js
@@ -20,14 +20,14 @@ const FeedbackModal = () => {
       <button id='feedbackButton' onClick={handleOpenModal} sx={sx.feedbackButton}>FEEDBACK</button>
       <ActionModal
         isOpen={settingsOpen}
-        contentLabel='Digital Collections Feedback'
+        contentLabel='Marble Feedback'
         shouldFocusAfterRender
         shouldReturnFocusAfterClose
         closeFunc={handleCloseModal}
       >
         <div>
           {/* Optional Google Form for backup */}
-          {/* <iframe title='Digital Collections Feedback' src='https://docs.google.com/forms/d/e/1FAIpQLSd_hrPoNy1SvGYnYAa8d9MQ5fNIQYpkxcHFFVu6rv8VmlFCgw/viewform?embedded=true' frameBorder='0' marginHeight='0' marginWidth='0'>Loading…</iframe> */}
+          {/* <iframe title='Marble Feedback' src='https://docs.google.com/forms/d/e/1FAIpQLSd_hrPoNy1SvGYnYAa8d9MQ5fNIQYpkxcHFFVu6rv8VmlFCgw/viewform?embedded=true' frameBorder='0' marginHeight='0' marginWidth='0'>Loading…</iframe> */}
           <FeedbackForm closeFunc={handleCloseModal} />
         </div>
       </ActionModal>

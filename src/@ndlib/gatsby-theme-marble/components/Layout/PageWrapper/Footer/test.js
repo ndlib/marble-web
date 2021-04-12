0f42f7a82b6e90efa48059d33e05381ce1868cdf
Jon Hartzler <jon.hartzler@gmail.com>
2021-04-12 09:49:44 -0400

    fix test

--- src/@ndlib/gatsby-theme-marble/components/Layout/PageWrapper/Footer/test.js
+++ src/@ndlib/gatsby-theme-marble/components/Layout/PageWrapper/Footer/test.js
@@ -2,10 +2,29 @@ import React from 'react'
 import { shallow } from 'enzyme'
 import Footer from './'
 import Menu from '@ndlib/gatsby-theme-marble/src/components/Shared/Menu'
+import { useStaticQuery } from 'gatsby'
+
+const sq = {
+  menusJson: {
+    label: 'footer',
+    id: 'footer',
+    items: [
+      {
+        id: 'footer',
+        label: 'label',
+        link: 'link',
+      },
+    ],
+  },
+}
 
 test('Footer', () => {
+  useStaticQuery.mockImplementationOnce(() => {
+    return sq
+  })
+
   const wrapper = shallow(<Footer />)
   expect(wrapper.find('.footer-email').at(0).text()).toEqual('sniteart@nd.edu')
   expect(wrapper.find('.footer-email').at(1).text()).toEqual('asklib@nd.edu')
-  expect(wrapper.find(Menu).props().menu).toEqual('footer')
+  expect(wrapper.find(Menu).props().variant).toEqual('footer')
 })

ab97c01fee149dd8a2008d82aaf8551a0ee4d239
Jon Hartzler <jon.hartzler@gmail.com>
2021-09-24 09:19:20 -0400

    remove metatag issues (#112)

--- src/components/Pages/IndexPage/SearchVerification/index.js
+++ src/components/Pages/IndexPage/SearchVerification/index.js
@@ -7,41 +7,14 @@ export const SearchVerification = () => {
       <meta
         name='google-site-verification'
         content='i5bUlLd7k9T3I_XoLupYuPOrZGkJ8BMSX9EIcEXjU0w'
-        userId='pw'
-      />
-      <meta
-        name='google-site-verification'
-        content='4G8TYzyR2-8KWRdA3F1LRXv9l1_sBvV3YS4UZ7115Po'
-        user='mk'
-      />
-      <meta
-        name='google-site-verification' content='qq7fBdp8zPSI3R9_LFQ4YFmBzTw7Etg5M5HbSgZ_hak'
-        user='rf'
-      />
-      <meta
-        name='google-site-verification'
-        content='zISdamiilUYTQJfVqU4OSM85zEDAJTcioljTdgbaCxo'
-        user='jh'
-      />
-      <meta
-        name='google-site-verification'
-        content='df7oJV_ryb0hmj991vCUHHgEkZs4_PxsqKbobpHEp8U'
-        user='rd'
-      />
-      <meta
-        name='google-site-verification'
-        content='kiUlnvDP0sZ96LCd5lRAbBvMxYVje53ppg2omqweX2Q'
-        user='dw'
       />
       <meta
         name='yandex-verification'
         content='26e8b10857c012b8'
-        user='pw'
       />
       <meta
         name='msvalidate.01'
         content='C307D552024FFC4966541DEABD413761'
-        user='pw'
       />
     </Helmet>
   )

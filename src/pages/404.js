a547d2077fee0334e6d3ec81f5b3bd54d18470e3
dan <dwolfe2@nd.edu>
2021-11-16 11:51:45 -0500

    MARBLE-2028 Fix layout of Marble 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
-import React from 'react'
+/** @jsx jsx */
+import { jsx, Heading } from 'theme-ui'
 import PropTypes from 'prop-types'
 import { I18nextProvider } from 'react-i18next'
 import i18next from '@ndlib/gatsby-theme-marble/src/i18n'
-import Layout from '../components/Layout'
+import Link from '@ndlib/gatsby-theme-marble/src/components/Shared/Link'
+import NDBrandSection from '@ndlib/gatsby-theme-marble/src/components/Shared/NDBrand/Section'
+import NDBrandBreadcrumbs from '@ndlib/gatsby-theme-marble/src/components/Shared/NDBrand/Breadcrumbs'
 import Seo from '@ndlib/gatsby-theme-marble/src/components/Shared/Seo'
+import Layout from '../components/Layout'
 
 const NotFoundPage = ({ location }) => {
   return (
@@ -18,7 +22,17 @@ const NotFoundPage = ({ location }) => {
         description='The requested page could not be found.'
       />
       <I18nextProvider i18n={i18next}>
-        <div>We&apos;re sorry. We seem to have lost a few marbles...</div>
+        <NDBrandSection location={location} sx={{ paddingBottom: '1rem' }}>
+          <NDBrandBreadcrumbs
+            currentPageTitle='404'
+            breadcrumbs={[]}
+          />
+          <Heading as='h1' variant='pageTitle'>Page not found</Heading>
+          <div sx={{ minHeight: '400px' }}>
+            <p>We&apos;re sorry. We seem to have lost a few marbles…</p>
+            <p>Perhaps you may find what you&apos;re looking for with a <Link to='/search?images[0]=true'>quick search.</Link></p>
+          </div>
+        </NDBrandSection>
       </I18nextProvider>
     </Layout>
 

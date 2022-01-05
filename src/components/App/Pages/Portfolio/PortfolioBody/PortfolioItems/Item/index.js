84a1999b930a9a97c658f3d5b5531100feff1ac6
dan <dwolfe2@nd.edu>
2022-01-05 15:31:02 -0500

    Update index.js

--- src/components/App/Pages/Portfolio/PortfolioBody/PortfolioItems/Item/index.js
+++ src/components/App/Pages/Portfolio/PortfolioBody/PortfolioItems/Item/index.js
@@ -41,7 +41,12 @@ const Item = ({ item, userId, isOwner, annotated = false }) => {
     return (
       <div sx={sx.item}>
         <Flex sx={{ flexWrap: 'wrap' }}>
-          <Box sx={{ width: ['100%', '60%', '60%'], px: '1rem', py: '1rem', whiteSpace: 'break-space' }}>
+          <Box sx={{
+            width: ['100%', '60%', '60%'],
+            px: '1rem',
+            py: '1rem',
+            whiteSpace: 'break-space',
+          }}>
             {item.annotation}
           </Box>
           <Box sx={{ width: ['100%', '40%'], py: '1rem' }}>
